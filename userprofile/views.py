472d60fa2d7fbf4238ea79e9bf8e73b06af5d2c9
cbstange <>
2024-02-06 18:15:25 +0000

    Define signup view

--- userprofile/views.py
+++ userprofile/views.py
@@ -1,9 +1,21 @@
-from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+from .models import Userprofile
 
 # Create your views here.
 
 def signup(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
 
-    return render(request, 'signup.html')
+        if form.is_valid():
+            user = form.save()
+            Userprofile.objects.create(user=user)
+
+            return redirect('/log-in/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {
+        'form': form
+    })
