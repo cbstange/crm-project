614eb1cc988980804515a803eacbe17d8fc40510
Cole Stange <cbstange@me.com>
2024-02-06 20:22:47 +0000

    Add logout funtionality

--- crmproject/urls.py
+++ crmproject/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth import views
-from django.urls import path
+from django.urls import path, include
 
 from core.views import index
 from userprofile.views import signup
@@ -8,6 +8,7 @@ from userprofile.views import signup
 urlpatterns = [
     path('', index, name='index'),
     path('sign-up/', signup, name='signup'),
-    path('log-in/', views.LoginView.as_view(template_name='login.html'), name='log-in'),
+    path('log-in/', views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('log-out/', views.LogoutView.as_view(), name='logout'),
     path('admin/', admin.site.urls),
 ]
