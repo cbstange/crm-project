fb6fdceb5629dd615570bb99c9908f9057275221
Cole Stange <cbstange@me.com>
2024-02-07 18:11:04 +0000

    Add form to handle new leads

--- lead/views.py
+++ lead/views.py
@@ -1,6 +1,21 @@
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import NewLeadForm
 
 @login_required
 def new_lead(request):
-    return render(request, 'new_lead.html')
+    if request.method == 'POST':
+        form = NewLeadForm(request.POST)
+
+        if form.is_valid():
+            lead = form.save(commit=False)
+            lead.created_by = request.user
+            lead.save()
+
+            return redirect('dashboard')
+    else:
+        form = NewLeadForm()   
+    return render(request, 'new_lead.html', {
+        'form': form
+    })
