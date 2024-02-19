c8d6cce5b93494411e7115b56f03ab6de684e41f
Cole Stange <cbstange@me.com>
2024-02-19 17:39:22 +0000

    Install Tailwind CLI

--- team/views.py
+++ team/views.py
@@ -1,11 +1,26 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 from .models import Team
+from .forms import TeamForm
 
 @login_required
 def edit_team(request, pk):
     team = get_object_or_404(Team, created_by=request.user, pk=pk)
 
+    if request.method == 'POST':
+        form = TestForm(request.POST, instance=team)
+
+        if form.is_valid():
+            form.save()
+
+            messages.success(request, 'The changes were saved.')
+
+            return redirect('myaccount')
+    else:
+        form = TeamForm(instance=team)
+
     return render(request, 'edit_team.html', {
-        'team': team
+        'team': team,
+        'form': form
     })
